f69e473c6af88c5cfd41edac17ce5071b7cf922c
Abdiaziz <145577421+0paziz@users.noreply.github.com>
2024-11-25 22:18:58 +0300

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,41 +1,26 @@
+let random_number = Math.floor(Math.random() * 100 + 1);
 
-var r1= Math.floor( Math.random()*100+1);
-
-
-var count=0;
-var g;
-
-
-document.getElementById("submit").addEventListener("click",function Getvalue(){
-    g = parseInt(document.getElementById("number").value);
-    process(g);
-})
-
-function process(na){
-
-    document.getElementById("correct").style.display = "none";
-    document.getElementById("l").style.display = "none";
-    document.getElementById("h").style.display = "none";
-    document.getElementById("Wrong").style.display = "none";
+$("#check").click(function() {
+    const guessed_number = Number($("#guess").val());
     
-    if (isNaN(g)) {
-        document.getElementById("Wrong").style.display = "block"; // Show invalid input message
+    if (!guessed_number || guessed_number < 1 || guessed_number > 100) {
+        $(".user_helper").text("Please enter a valid number between 1 and 100.");
+        return;
     }
-else if(r1===g){
 
-    document.getElementById("correct").style.display="block";
-  
-}
+    if (random_number > guessed_number) {
+        $(".user_helper").text("My secret number is greater than " + guessed_number);
+    } else if (random_number < guessed_number) {
+        $(".user_helper").text("My secret number is less than " + guessed_number);
+    } else {
+        $(".user_helper").css("color", "green").text("Correct, it's " + guessed_number + "!");
+    }
 
-else if(r1>g){
-    document.getElementById("l").style.display="block";
-  }
+    $("#guess").val("");
+});
 
-else if(r1<g){
-    document.getElementById("h").style.display="block";
-  }
-  else{
-    document.getElementById("Wrong").style.display="block";
-  }
-  
-}
+$("#again").click(function() {
+    random_number = Math.floor(Math.random() * 100 + 1); // Reset the random number
+    $(".user_helper").text("").css("color", ""); // Reset feedback
+    $("#guess").val(""); // Clear input field
+});
