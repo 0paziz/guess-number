7eb481a87abe1c58b9b32a1f4b5eda14e0dfbb64
Abdiaziz <145577421+0paziz@users.noreply.github.com>
2024-12-11 14:02:35 +0300

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,4 +1,9 @@
 let random_number = Math.floor(Math.random() * 100 + 1);
+let score_id= $("#score");
+let highscore_id= $("#highscore");
+let highscore=0;
+let Score= 0;
+
 
 $("#check").click(function() {
     const guessed_number = Number($("#guess").val());
@@ -7,14 +12,22 @@ $("#check").click(function() {
         $(".user_helper").text("Please enter a valid number between 1 and 100.");
         return;
     }
-
+    
     if (random_number > guessed_number) {
         $(".user_helper").text("My secret number is greater than " + guessed_number);
     } else if (random_number < guessed_number) {
         $(".user_helper").text("My secret number is less than " + guessed_number);
     } else {
+        Score += 20;
         $(".user_helper").css("color", "green").text("Correct, it's " + guessed_number + "!");
+        score_id.txt(Score);
+        if(Score > highscore){
+            highscore=Score;
+            
+        }
+        highscore_id.text(highscore);
     }
+    
 
     $("#guess").val("");
 });
